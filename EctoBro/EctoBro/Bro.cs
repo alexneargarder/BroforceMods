5f00a4291a4082979239c57da87e09211913acb7
Alex Neargarder <alexneargarder@yahoo.com>
2024-01-04 15:48:52 -0500

    Added sprites for EctoBro and Mission Impossibro

---
+++ EctoBro/EctoBro/Bro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BroMakerLib;
+using BroMakerLib.CustomObjects.Bros;
+
+namespace EctoBro
+{
+    [HeroPreset("EctoBro", HeroType.Rambro)]
+    public class Bro : CustomHero
+    {
+    }
+}
