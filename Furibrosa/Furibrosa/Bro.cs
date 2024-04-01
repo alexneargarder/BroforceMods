bcef60a5fd48c1021560ca5a05816c1122c39f58
Alex Neargarder <alexneargarder@yahoo.com>
2024-04-01 18:52:53 -0400

    Furibrosa - Setting up bro

---
+++ Furibrosa/Furibrosa/Bro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using BroMakerLib;
+using BroMakerLib.CustomObjects.Bros;
+using BroMakerLib.Loggers;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace Furibrosa
+{
+    [HeroPreset("Furibrosa", HeroType.Rambro)]
+    public class Bro : CustomHero
+    {
+    }
+}
