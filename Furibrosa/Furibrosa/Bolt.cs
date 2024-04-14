f3159b3ae28279d85a7939774c0b5d4774564a68
Alex Neargarder <alexneargarder@yahoo.com>
2024-04-14 02:06:25 -0400

    Furibrosa - Started working on crossbow

---
+++ Furibrosa/Furibrosa/Bolt.cs
@@ -0,0 +1,106 @@
+﻿using BroMakerLib;
+using BroMakerLib.Loggers;
+using RocketLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+
+namespace Furibrosa
+{
+    public class Bolt : PredabroSpear
+    {
+        public static Material storedMat;
+        protected SpriteSM sprite;
+
+        protected override void Awake()
+        {
+            this.canMakeEffectsMoreThanOnce = true;
+            this.stunOnHit = false;
+            this.groundLayer = (1 << LayerMask.NameToLayer("Ground") | 1 << LayerMask.NameToLayer("LargeObjects") | 1 << LayerMask.NameToLayer("FLUI"));
+            this.barrierLayer = (1 << LayerMask.NameToLayer("MobileBarriers") | 1 << LayerMask.NameToLayer("IndestructibleGround"));
+            this.friendlyBarrierLayer = 1 << LayerMask.NameToLayer("FriendlyBarriers");
+            this.fragileLayer = 1 << LayerMask.NameToLayer("DirtyHippie");
+            this.zOffset = (1f - UnityEngine.Random.value * 2f) * 0.04f;
+            this.random = new Randomf(UnityEngine.Random.Range(0, 10000));
+        }
+
+        private void OnDisable()
+        {
+            // Do nothing
+        }
+
+        public void Setup()
+        {
+            MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
+
+            string directoryPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (storedMat == null)
+            {
+                storedMat = ResourcesController.GetMaterial(directoryPath, "bolt.png");
+            }
+
+            renderer.material = storedMat;
+
+            this.sprite = this.gameObject.GetComponent<SpriteSM>();
+            sprite.lowerLeftPixel = new Vector2(-3, 16);
+            sprite.pixelDimensions = new Vector2(17, 16);
+
+            sprite.plane = SpriteBase.SPRITE_PLANE.XY;
+            sprite.width = 17;
+            sprite.height = 16;
+            sprite.offset = new Vector3(-10.5f, 0f, 10.82f);
+
+            // Setup collider
+            BoxCollider collider = this.gameObject.GetComponent<BoxCollider>();
+            collider.center = new Vector3(0f, 0.5f, 8f);
+            collider.size = new Vector3(26f, 2f, 0f);
+
+            // Setup Variables
+            this.groundLayersCrushLeft = 1;
+            this.maxPenetrations = 1;
+            this.unimpalementDamage = 8;
+            this.trailDist = 12;
+            this.stunOnHit = false;
+            this.rotationSpeed = 0;
+            this.dragUnitsSpeedM = 0.9f;
+            this.projectileSize = 8;
+            this.damage = 5;
+            this.damageInternal = 5;
+            this.fullDamage = 5;
+            this.fadeDamage = false;
+            this.damageType = DamageType.Bullet;
+            this.canHitGrenades = true;
+            this.affectScenery = true;
+            this.horizontalProjectile = true;
+            this.isWideProjectile = false;
+            this.canReflect = true;
+            this.canMakeEffectsMoreThanOnce = true;
+
+            // Setup platform collider
+            this.platformCollider = this.FindChildOfName("BoltCollider").gameObject.GetComponent<BoxCollider>();
+            this.platformCollider.material = new PhysicMaterial();
+            this.platformCollider.material.dynamicFriction = 0.6f;
+            this.platformCollider.material.staticFriction = 0.6f;
+            this.platformCollider.material.bounciness = 0f;
+            this.platformCollider.material.frictionCombine = PhysicMaterialCombine.Average;
+            this.platformCollider.material.bounceCombine = PhysicMaterialCombine.Average;
+            this.platformCollider.gameObject.layer = 15;
+
+            // Setup foreground sprite
+            GameObject foreground = this.FindChildOfName("BoltForeground").gameObject;
+            foreground.GetComponent<MeshRenderer>().material = storedMat;
+            SpriteSM foregroundSprite = foreground.GetComponent<SpriteSM>();
+            foregroundSprite.lowerLeftPixel = new Vector2(14, 16);
+            foregroundSprite.pixelDimensions = new Vector2(14, 16);
+            foregroundSprite.plane = SpriteBase.SPRITE_PLANE.XY;
+            foregroundSprite.width = 14;
+            foregroundSprite.height = 16;
+            foregroundSprite.offset = new Vector3(-1f, 0f, 0f);
+            foreground.transform.localPosition = new Vector3(5f, 0f, -0.62f);
+        }
+    }
+}
---
+++ Furibrosa/Furibrosa/Furibrosa.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using BroMakerLib;
+using BroMakerLib.CustomObjects.Bros;
+using BroMakerLib.Loggers;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using RocketLib;
+using Rogueforce;
+
+namespace Furibrosa
+{
+    [HeroPreset("Furibrosa", HeroType.Rambro)]
+    public class Furibrosa : CustomHero
+    {
+        // General
+        public static KeyBindingForPlayers switchWeaponKey;
+
+        // Primary
+        protected Bolt boltPrefab;
+        protected bool releasedFire = false;
+
+        // Melee
+
+        // Special
+
+        protected bool acceptedDeath = false;
+        bool wasInvulnerable = false;
+
+        #region General
+        protected override void Awake()
+        {
+            if (switchWeaponKey == null )
+            {
+                LoadKeyBinding();
+            }
+
+            base.Awake();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+
+            boltPrefab = new GameObject("Bolt", new Type[] { typeof(Transform), typeof(MeshFilter), typeof(MeshRenderer), typeof(SpriteSM), typeof(BoxCollider), typeof(Bolt)}).GetComponent<Bolt>();
+            boltPrefab.enabled = false;
+            boltPrefab.soundHolder = (HeroController.GetHeroPrefab(HeroType.Predabro) as Predabro).projectile.soundHolder;
+
+            BoxCollider collider = new GameObject("BoltCollider", new Type[] { typeof(Transform), typeof(BoxCollider) }).GetComponent<BoxCollider>();
+            collider.enabled = false;
+            collider.transform.parent = boltPrefab.transform;
+
+            Transform transform = new GameObject("BoltForeground", new Type[] { typeof(Transform), typeof(MeshFilter), typeof(MeshRenderer), typeof(SpriteSM) }).transform;
+            transform.parent = boltPrefab.transform;
+
+            boltPrefab.Setup();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (this.acceptedDeath)
+            {
+                if (this.health <= 0 && !this.WillReviveAlready)
+                {
+                    return;
+                }
+                // Revived
+                else
+                {
+                    this.acceptedDeath = false;
+                }
+            }
+
+            if (this.invulnerable)
+            {
+                this.wasInvulnerable = true;
+            }
+
+            // Check if invulnerability ran out
+            if (this.wasInvulnerable && !this.invulnerable)
+            {
+                // Fix any not currently displayed textures
+            }
+        }
+
+        public static void LoadKeyBinding()
+        {
+            if ( !AllModKeyBindings.TryGetKeyBinding("Furibrosa", "Switch Weapon", out switchWeaponKey) )
+            {
+                switchWeaponKey = new KeyBindingForPlayers("Switch Weapon", "Furibrosa");
+            }
+        }
+
+        public override void UIOptions()
+        {
+            if (switchWeaponKey == null)
+            {
+                LoadKeyBinding();
+            }
+            int player;
+
+            GUILayout.Space(10);
+            switchWeaponKey.OnGUI(out player, true);
+            GUILayout.Space(10);
+        }
+        #endregion
+
+        #region Primary
+        protected override void ReleaseFire()
+        {
+            this.releasedFire = true;
+            base.ReleaseFire();
+        }
+
+        protected override void RunFiring()
+        {
+            if (this.health <= 0)
+            {
+                return;
+            }
+            if (this.fire)
+            {
+                this.StopRolling();
+            }
+            this.fireCounter += this.t;
+            if (this.releasedFire && this.fireCounter >= this.fireRate)
+            {
+                if (this.player != null && this.player.ValueOrchestrator != null)
+                {
+                    this.fireCounter -= this.player.ValueOrchestrator.GetModifiedFloatValue(ValueOrchestrator.ModifiableType.TimeBetweenShots, this.fireRate);
+                }
+                else
+                {
+                    this.fireCounter -= this.fireRate;
+                }
+                this.UseFire();
+                this.FireFlashAvatar();
+                this.SetGestureAnimation(GestureElement.Gestures.None);
+            }
+        }
+
+        protected override void UseFire()
+        {
+            if (this.doingMelee)
+            {
+                this.CancelMelee();
+            }
+            this.releasedFire = false;
+            float num = base.transform.localScale.x;
+            if (!base.IsMine && base.Syncronize)
+            {
+                num = (float)this.syncedDirection;
+            }
+            if (Connect.IsOffline)
+            {
+                this.syncedDirection = (int)base.transform.localScale.x;
+            }
+            this.FireWeapon(base.X + num * 10f, base.Y + 8f, num * 300f, 0);
+            //this.PlayAttackSound();
+            Map.DisturbWildLife(base.X, base.Y, 60f, base.playerNum);
+        }
+
+        protected override void FireWeapon(float x, float y, float xSpeed, float ySpeed)
+        {
+            this.gunFrame = 3;
+            this.SetGunSprite(this.gunFrame, 0);
+            this.TriggerBroFireEvent();
+            EffectsController.CreateMuzzleFlashEffect(x, y, -25f, xSpeed * 0.15f, ySpeed * 0.15f, base.transform);
+            Bolt firedBolt = ProjectileController.SpawnProjectileLocally(boltPrefab, this, x, y, xSpeed, ySpeed, base.playerNum) as Bolt;
+            firedBolt.enabled = true;
+        }
+
+        protected override void RunGun()
+        {
+            if (!this.WallDrag && this.gunFrame > 0)
+            {
+                this.gunCounter += this.t;
+                if (this.gunCounter > 0.0334f)
+                {
+                    this.gunCounter -= 0.0334f;
+                    this.gunFrame--;
+                    this.SetGunSprite(this.gunFrame, 0);
+                }
+            }
+        }
+        #endregion
+
+        #region Melee
+        #endregion
+
+        #region Special
+        #endregion
+    }
+}
