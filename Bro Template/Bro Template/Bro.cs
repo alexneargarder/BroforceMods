6322e62c622bfbc982accc6c9c00230b19013a9a
Alex Neargarder <alexneargarder@yahoo.com>
2024-04-14 02:05:54 -0400

    Updated Bro Template

--- Bro Template/Bro Template/Bro.cs
+++ Bro Template/Bro Template/Bro.cs
@@ -12,5 +12,54 @@ namespace Bro_Template
     [HeroPreset("Bro Template", HeroType.Rambro)]
     public class Bro : CustomHero
     {
+        // General
+
+        // Primary
+
+        // Melee
+
+        // Special
+
+        protected bool acceptedDeath = false;
+        bool wasInvulnerable = false;
+
+        #region General
+        protected override void Update()
+        {
+            base.Update();
+            if (this.acceptedDeath)
+            {
+                if (this.health <= 0 && !this.WillReviveAlready)
+                {
+                    return;
+                }
+                // Revived
+                else
+                {
+                    this.acceptedDeath = false;
+                }
+            }
+
+            if (this.invulnerable)
+            {
+                this.wasInvulnerable = true;
+            }
+
+            // Check if invulnerability ran out
+            if (this.wasInvulnerable && !this.invulnerable)
+            {
+                // Fix any not currently displayed textures
+            }
+        }
+        #endregion
+
+        #region Primary
+        #endregion
+
+        #region Melee
+        #endregion
+
+        #region Special
+        #endregion
     }
 }
