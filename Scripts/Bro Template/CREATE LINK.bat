80330f3c5e57ce764dba7572eb3f5cf9a005289e
Alex Neargarder <alexneargarder@yahoo.com>
2025-06-12 21:38:43 -0400

    Move files

--- Scripts/Bro Template/CREATE LINK.bat
+++ Scripts/Bro Template/CREATE LINK.bat
@@ -1,2 +1,2 @@
-mklink "%BROPATH%\Bro Template\Bro Template.dll" "C:\Users\Alex\Desktop\Coding Things\Github\BroforceModsDev\Bro Template\Bro Template\bin\Debug\Bro Template.dll"
+mklink "%BROPATH%\Bro Template\Bro Template.dll" "%REPOS%\BroforceMods\Bro Template\Bro Template\bin\Debug\Bro Template.dll"
 pause
--- Scripts/Mod Install Template/CREATE LINK.bat
+++ Scripts/Mod Install Template/CREATE LINK.bat
@@ -1,2 +1,2 @@
-mklink "D:\Steam\steamapps\common\Broforce\Mods\Mod Template\Mod Template.dll" "C:\Users\Alex\Desktop\Coding Things\Github\BroforceModsDev\Mod Template\Mod Template\bin\Release\Mod Template.dll"
+mklink "%BROFORCEMODS%\Mod Template\Mod Template.dll" "C:\Users\Alex\Desktop\Coding Things\Github\BroforceModsDev\Mod Template\Mod Template\bin\Release\Mod Template.dll"
 pause
